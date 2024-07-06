5758cab269723bb9e0585a2afe3b40389faa05f2
Jakub Zawada <JakubZawadaa3@gmail.com>
2024-07-06 15:37:19 +0200

    Implement SharedPreferences for Notifications

--- lib/widgets/notification_settings.dart
+++ lib/widgets/notification_settings.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/foundation.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class NotificationSettings extends ChangeNotifier {
   bool _dropIsSwitched = true;
@@ -6,6 +7,10 @@ class NotificationSettings extends ChangeNotifier {
   bool _womanSaleIsSwitched = true;
   bool _otherIsSwitched = true;
   bool _darkModeIsSwitched = false;
+
+  NotificationSettings() {
+    _loadPreferences();
+  }
 
   bool get dropIsSwitched => _dropIsSwitched;
   bool get manSaleIsSwitched => _manSaleIsSwitched;
@@ -15,26 +20,50 @@ class NotificationSettings extends ChangeNotifier {
 
   void toggleDrop(bool value) {
     _dropIsSwitched = value;
+    _savePreferences();
     notifyListeners();
   }
 
   void toggleManSale(bool value) {
     _manSaleIsSwitched = value;
+    _savePreferences();
     notifyListeners();
   }
 
   void toggleWomanSale(bool value) {
     _womanSaleIsSwitched = value;
+    _savePreferences();
     notifyListeners();
   }
 
   void toggleOther(bool value) {
     _otherIsSwitched = value;
+    _savePreferences();
     notifyListeners();
   }
 
   void toggleDarkMode(bool value) {
     _darkModeIsSwitched = value;
+    _savePreferences();
+    notifyListeners();
+  }
+
+  Future<void> _savePreferences() async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setBool('dropIsSwitched', _dropIsSwitched);
+    prefs.setBool('manSaleIsSwitched', _manSaleIsSwitched);
+    prefs.setBool('womanSaleIsSwitched', _womanSaleIsSwitched);
+    prefs.setBool('otherIsSwitched', _otherIsSwitched);
+    prefs.setBool('darkModeIsSwitched', _darkModeIsSwitched);
+  }
+
+  Future<void> _loadPreferences() async {
+    final prefs = await SharedPreferences.getInstance();
+    _dropIsSwitched = prefs.getBool('dropIsSwitched') ?? true;
+    _manSaleIsSwitched = prefs.getBool('manSaleIsSwitched') ?? true;
+    _womanSaleIsSwitched = prefs.getBool('womanSaleIsSwitched') ?? true;
+    _otherIsSwitched = prefs.getBool('otherIsSwitched') ?? true;
+    _darkModeIsSwitched = prefs.getBool('darkModeIsSwitched') ?? false;
     notifyListeners();
   }
 }
