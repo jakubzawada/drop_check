04d86a66ad3cb06df5782391c495270a7acb1302
Jakub Zawada <JakubZawadaa3@gmail.com>
2024-07-06 17:00:41 +0200

    Filter notifications based on user preferences

--- lib/widgets/notification_settings.dart
+++ lib/widgets/notification_settings.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
 class NotificationSettings extends ChangeNotifier {
   bool _dropIsSwitched = true;
@@ -21,24 +22,28 @@ class NotificationSettings extends ChangeNotifier {
   void toggleDrop(bool value) {
     _dropIsSwitched = value;
     _savePreferences();
+    _updateFCMSubscription('drop', value);
     notifyListeners();
   }
 
   void toggleManSale(bool value) {
     _manSaleIsSwitched = value;
     _savePreferences();
+    _updateFCMSubscription('manSale', value);
     notifyListeners();
   }
 
   void toggleWomanSale(bool value) {
     _womanSaleIsSwitched = value;
     _savePreferences();
+    _updateFCMSubscription('womanSale', value);
     notifyListeners();
   }
 
   void toggleOther(bool value) {
     _otherIsSwitched = value;
     _savePreferences();
+    _updateFCMSubscription('other', value);
     notifyListeners();
   }
 
@@ -65,5 +70,13 @@ class NotificationSettings extends ChangeNotifier {
     _otherIsSwitched = prefs.getBool('otherIsSwitched') ?? true;
     _darkModeIsSwitched = prefs.getBool('darkModeIsSwitched') ?? false;
     notifyListeners();
+  }
+
+  Future<void> _updateFCMSubscription(String topic, bool subscribe) async {
+    if (subscribe) {
+      await FirebaseMessaging.instance.subscribeToTopic(topic);
+    } else {
+      await FirebaseMessaging.instance.unsubscribeFromTopic(topic);
+    }
   }
 }
