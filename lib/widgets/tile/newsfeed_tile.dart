036f026bc89706198245beac8680518f8059ade8
Jakub Zawada <JakubZawadaa3@gmail.com>
2024-07-28 18:07:29 +0200

    Add bug fixes with images in NewsPage

--- lib/widgets/tile/newsfeed_tile.dart
+++ lib/widgets/tile/newsfeed_tile.dart
@@ -40,8 +40,8 @@ class NewsfeedTile extends StatelessWidget {
           borderRadius: BorderRadius.circular(12),
           child: AspectRatio(
             aspectRatio: 16 / 9,
-            child: Image.asset(
-              'lib/images/${newsfeed.imagePath}',
+            child: Image.network(
+              newsfeed.imagePath,
               fit: BoxFit.cover,
             ),
           ),
